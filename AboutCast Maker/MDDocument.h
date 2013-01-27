eafffb24a644fe9f36fa8d4009359e04f5d827f3
Dimitri Bouniol <dimitribouniol@mochidev.com>
2013-01-27 02:11:51 -0800

    Started work for App Showcase

---
+++ AboutCast Maker/MDDocument.h
@@ -0,0 +1,13 @@
+//
+//  MDDocument.h
+//  AboutCast Maker
+//
+//  Created by Dimitri Bouniol on 1/27/13.
+//  Copyright (c) 2013 Mochi Development, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MDDocument : NSDocument
+
+@end
---
+++ AboutCast Maker/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  AboutCast Maker
+//
+//  Created by Dimitri Bouniol on 1/27/13.
+//  Copyright (c) 2013 Mochi Development, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
